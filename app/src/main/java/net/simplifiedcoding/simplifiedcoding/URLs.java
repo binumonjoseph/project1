446d60891f710e96c71399be845f384ee04ac2f6
binumonjoseph <binumonjoseph2010@gmail.com>
2021-05-27 09:58:17 +0530

    initial commit of android app

---
+++ app/src/main/java/net/simplifiedcoding/simplifiedcoding/URLs.java
@@ -0,0 +1,10 @@
+package net.simplifiedcoding.simplifiedcoding;
+
+public class URLs {
+
+    private static final String ROOT_URL = "https://www.aesajce.in/moodle/androidws/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN = ROOT_URL + "login";
+
+}
